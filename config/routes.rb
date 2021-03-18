24b813dcfd38464b9a594f8aebcb7dfe25437acd
Katherine Nguyen <khnguyen86@live.com>
2021-03-17 18:19:32 -0700

    adds authenticated method for dashboard/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  authenticated :user do
+    root 'dashboard#home', as: :authenticated_root
+  end
   root 'dashboard#home'
   get 'dashboard/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
